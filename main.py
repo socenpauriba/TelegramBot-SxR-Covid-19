2d2b2c4ba4cd818da529984d55047d3c6c7b5bad
Pau Riba <jo@pribaq.net>
2020-05-22 17:38:50 +0200

    Revert "Avisos al administrador"
    This reverts commit 7d7f4178432f1e1c49859cd00eb61d04a1ea0afc.

--- main.py
+++ main.py
@@ -18,7 +18,6 @@ NOM, TELEFON, CENTRE, ADRECA, MASCARETES, GORROS, BATES, GEL, PANTALLES, GUANTS 
 NOMCC, TELEFONCC, CEDIR = range(3)
 
 canal = "CODI DE GRUP/CANAL/USUARI DE TELEGRAM" # Allà s'enviaran les respostes dels formularis
-admin = "CODI DE GRUP/CANAL/USUARI DE TELEGRAM" # Allà s'enviaran les alertes (Inici del bot)
 enter = """
 """
 nomc = ""
@@ -249,7 +248,6 @@ def main():
     updater.start_polling()
     updater.idle()
 
-    context.bot.send_message(chat_id=admin, text="⚙️ El bot s'ha iniciat correctament!", parse_mode='MARKDOWN')
 
 `#LOOP
 
